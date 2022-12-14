0ffd25525cd697d346dbbee2b722c14c4f896b2d
Rish2911 <rsingh24@umd.edu>
2022-12-14 01:12:58 -0500

    2.6[ADD] implemented the move to goal function

--- src/robot.cpp
+++ src/robot.cpp
@@ -51,3 +51,96 @@ if (!m_msg->transforms.empty())
   }
  }
 }
+
+void acme::Robot::move_to_goal(std::array<std::array<double, 2>, 5> goal_loc)
+{
+ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+ ros::NodeHandle robot_nh_; 
+
+  bool goal_sent = false;
+  int i = 0;
+  tf2_ros::Buffer tfBuffer; //Stores known frames and offers a ROS service, "tf_frames"
+
+  //provides an easy way to request and receive coordinate frame transform information.
+  tf2_ros::TransformListener tfListener(tfBuffer); 
+  ros::Publisher robot_velocity_publisher;
+  ros::Subscriber fiducial_reader;
+  geometry_msgs::Twist msg;
+
+ //checking if the robot is explorer or the follower
+  if(robot_name_.compare("explorer") == 0)
+  {
+    //building a velocity publisher 
+    
+    msg.linear.x = 0;
+    msg.angular.z = 0.15;
+    // move the bot with a constant angular velocity using node handle
+    robot_velocity_publisher = robot_nh_.advertise<geometry_msgs::Twist>("/explorer/cmd_vel", 10);
+   
+  }
+  
+   
+   robot_goal_.target_pose.header.frame_id = "map";
+   robot_goal_.target_pose.header.stamp = ros::Time::now();
+   robot_goal_.target_pose.pose.position.x = goal_loc.at(i).at(0);
+   robot_goal_.target_pose.pose.position.y = goal_loc.at(i).at(1);
+   robot_goal_.target_pose.pose.orientation.w = 1.0;
+   i++;  
+
+  while (!robot_client_.waitForServer(ros::Duration(5.0))) {
+    ROS_INFO_STREAM("Waiting for the move_base action server to come up for "<< robot_name_);
+  }
+  
+  
+  ros::Rate loop_rate(10);
+  
+  while (ros::ok()) 
+  {
+      if (!goal_sent) {
+      ROS_INFO_STREAM("Sending goal to "<<robot_name_);
+      robot_client_.sendGoal(robot_goal_);//this should be sent only once per goal
+      robot_client_.waitForResult();
+      goal_sent = true;
+    }
+    if (robot_client_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+     
+      if  (robot_goal_.target_pose.pose.position.x == -4)
+      {
+        ROS_INFO_STREAM( robot_name_<<" robot reached home.");
+        break;
+      }
+  
+      else
+      {
+       ROS_INFO_STREAM(robot_name_<<" robot reached goal" );
+       if(robot_name_.compare("explorer") == 0)
+      {
+        
+        ROS_INFO("Begin scan");
+        fiducial_reader = robot_nh_.subscribe("/fiducial_transforms", 100, &acme::Robot::aruco_callback, this);
+        while (saw_marker != true)
+          {
+           robot_velocity_publisher.publish(msg);
+           ros::spinOnce();
+          }
+        fiducial_reader.shutdown();
+        saw_marker = false;
+        listen(tfBuffer);
+      }
+       
+       goal_sent = false;
+       
+       robot_goal_.target_pose.header.frame_id = "map";
+       robot_goal_.target_pose.header.stamp = ros::Time::now();
+       robot_goal_.target_pose.pose.position.x = goal_loc.at(i).at(0);
+       robot_goal_.target_pose.pose.position.y = goal_loc.at(i).at(1);
+       robot_goal_.target_pose.pose.orientation.w = 1.0;
+       i++;
+      }  
+     
+    }
+  
+    loop_rate.sleep();
+  }
+ 
+}
